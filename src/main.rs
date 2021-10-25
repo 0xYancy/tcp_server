a007b51ef5a89d8e2bb1265f7810465ea603b405
TiE <379705884@qq.com>
2021-10-25 19:57:29 +0800

    Basic reference

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+/*
+## author Yancy 
+## date 2021-10-26
+*/
+
+// import std io for dealing with error
+use std::io::{self, Read, Write};
+// import std net for network
+use std::net::{TcpListener, TcpStream};
+// import std thread for thread processing
+use std::thread;
+// import std time for time
+use std::time;
+
+//Create a method to handle the client
+fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    let mut buf = [0; 512];
+    for _ in 0..1000 {
+        let bytes_read = stream.read(&mut buf)?;
+        if bytes_read == 0 {
+            return Ok(());
+        }
+
+        stream.write(&buf[..bytes_read])?;
+        thread::sleep(time::Duration::from_secs(1));
+    }
+    Ok(())
+}
+
+//
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    let mut thread_vec: Vec<thread::JoinHandle<()>> = Vec::new();
+
+    for stream in listener.incoming() {
+        let stream = stream.expect("failed");
+        let handle = thread::spawn(move || {
+            handle_client(stream).unwrap_or_else(|error| eprintln!("{:?}",error));
+        });
+        thread_vec.push(handle);
+    }
+
+    for handle in thread_vec {
+        handle.join().unwrap();
+    }
+
+    Ok(())
 }
